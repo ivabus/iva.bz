c0b36575412ffab418fa0a168b45078a482f9811
Ivan Bushchik <ivabus@ivabus.dev>
2023-09-10 12:43:01 +0300

    Fixup for urouter 0.2.0
    Signed-off-by: Ivan Bushchik <ivabus@ivabus.dev>

--- prepare.sh
+++ prepare.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 set -e
 
-git clone https://github.com/ivabus/urouter
+# Pick only release version
+TAG=0.2.0
+git clone https://github.com/ivabus/urouter -b $TAG
 cd urouter
+
+# Download routable files
 curl -fsSL -o dotfiles https://raw.githubusercontent.com/ivabus/dotfiles/main/init
 curl -fsSL -o nix https://raw.githubusercontent.com/ivabus/dotfiles/main/init_nix
-curl -fsSL -o suse https://raw.githubusercontent.com/ivabus/asahi-opensuse/main/installer/bootstrap.sh
 curl -fsSL -o env https://raw.githubusercontent.com/ivabus/dotfiles/main/init_env
 
+# Setup routing
 cat >> alias.json <<EOF
 [
 	{
-		"uri": "d",
-		"alias": "dotfiles"
+		"uri": "/",
+		"alias": "https://ivabus.dev",
+		"is_url": true
 	},
 	{
-		"uri": "s",
-		"alias": "suse"
+		"uri": "/",
+		"alias": "dotfiles",
+		"curl_only": true
+	},
+	{
+		"uri": "d",
+		"alias": "dotfiles"
 	},
 	{
 		"uri": "e",
